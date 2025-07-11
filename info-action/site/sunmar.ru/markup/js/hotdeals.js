df2025b8eb5ea147873b1d712c562cd383b86487
annaksv <anna.vll@mail.ru>
2025-07-11 14:45:21 +0300

    Update hotdeals.js

--- info-action/site/sunmar.ru/markup/js/hotdeals.js
+++ info-action/site/sunmar.ru/markup/js/hotdeals.js
@@ -1,31 +1,33 @@
 import { hostReactAppReady } from "../../../common/js/usefuls.js";
+import { tag } from "./data.js";
 
 (async () => {
   await hostReactAppReady();
   
   const buttons = document.querySelectorAll('.navbar-button');
   
-  const categories = ['all', 'recommended', 'hotels', 'destinations', 'sunmarbonus', 'other'];
- 
+  const handleButtonClick = (clickedButton, categoryIndex) => {
+    buttons.forEach(btn => 
+        btn.classList.remove('active')
+    );
+
+    clickedButton.classList.add('active');
+    
+    window.dispatchEvent(new CustomEvent('filterPromos', {
+       
+      detail: { category: Object.values(tag)[categoryIndex] }
+    }));
+  };
+
   if (buttons.length > 0) {
-      buttons[0].classList.add('active');
+    buttons[0].classList.add('active');
   }
- 
+
   buttons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-          buttons.forEach(btn => {
-              btn.classList.remove('active');
-          });
- 
-          button.classList.add('active');
- 
-          window.dispatchEvent(new CustomEvent('filterPromos', {
-              detail: { category: categories[index] }
-          }));
-      });
+    button.addEventListener('click', () => handleButtonClick(button, index));
   });
- 
+   
   window.dispatchEvent(new CustomEvent('filterPromos', {
-      detail: { category: 'all' }
+    detail: { category: tag.all }  
   }));
 })();
