c88637eabe8f7ad477dad0acd838151ce882aba2
Алимбек <balimbek2006@mail.ru>
2025-07-14 15:17:07 +0300

    Reapply "вернул стандартную фильтрацию, и убрал секунды"
    This reverts commit cc1618d3845610349a1759f85f0a1f97253365a3.

--- info-action/site/sunmar.ru/markup/js/hotdeals.js
+++ info-action/site/sunmar.ru/markup/js/hotdeals.js
@@ -1,33 +1,31 @@
 import { hostReactAppReady } from "../../../common/js/usefuls.js";
-import { tag } from "./data.js";
 
 (async () => {
   await hostReactAppReady();
   
   const buttons = document.querySelectorAll('.navbar-button');
   
-  const handleButtonClick = (clickedButton, categoryIndex) => {
-    buttons.forEach(btn => 
-        btn.classList.remove('active')
-    );
-
-    clickedButton.classList.add('active');
-    
-    window.dispatchEvent(new CustomEvent('filterPromos', {
-       
-      detail: { category: Object.values(tag)[categoryIndex] }
-    }));
-  };
-
+  const categories = ['all', 'recommended', 'hotels', 'destinations', 'sunmarBonus', 'other'];
+ 
   if (buttons.length > 0) {
-    buttons[0].classList.add('active');
+      buttons[0].classList.add('active');
   }
-
+ 
   buttons.forEach((button, index) => {
-    button.addEventListener('click', () => handleButtonClick(button, index));
+      button.addEventListener('click', () => {
+          buttons.forEach(btn => {
+              btn.classList.remove('active');
+          });
+ 
+          button.classList.add('active');
+ 
+          window.dispatchEvent(new CustomEvent('filterPromos', {
+              detail: { category: categories[index] }
+          }));
+      });
   });
-   
+ 
   window.dispatchEvent(new CustomEvent('filterPromos', {
-    detail: { category: tag.all }  
+      detail: { category: 'all' }
   }));
 })();
